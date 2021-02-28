ddb8e831364a3da3817c39cbd557c385242132e6
yolmaryda <yolmaryda@gmail.com>
2021-02-28 13:39:19 -0400

    Primera actualizacion

---
+++ app.js
@@ -0,0 +1,9 @@
+// function myFunction() {
+//     var x = document.getElementById("myNavigation");
+//     if (x.className === "navigation") {
+//       x.className += " responsive";
+//     } else {
+//       x.className = "navigation";
+//     }
+// }
+
